793fed2655f86015cbc1387eaf958c6c6b685097
Edward Parks <edward.parks@gmail.com>
2020-06-19 09:42:02 -0700

    Finished Builder coding exercise.

--- designPatterns.md
+++ designPatterns.md
@@ -2,39 +2,45 @@
     -Deal with the creation (construction) of objects
     -Explicit (constructor) vs. implicit (DI, refelection, etc.)
     -Wholesale (single statement) vs piecewise (step-by-step)
-#### Builder  
+#### Builder
     -When piecewise object construction is complicated,
     provide an API for doing it succinctly
-#### Factories  
-    -- Abstract Factory  
-    -- Factory Method  
-#### Prototype 
+#### Factories
+    Factory: A component responsible solely
+             for the wholesale (not piecewise)
+             creation of objects.
+
+    -- Abstract Factory
+    -- Factory Method
+#### Prototype
 #### Singleton
+
 
 ## Behavioral
     -They're all different; no central theme
-#### Chain of Responsibility  
-#### Command  
-#### Interpreter  
-#### Iterator  
-#### Mediator  
-#### Memento  
-#### Null Object  
-#### Observer  
-#### State  
-#### Strategy  
-#### Template Method  
-#### Visitor  
+#### Chain of Responsibility
+#### Command
+#### Interpreter
+#### Iterator
+#### Mediator
+#### Memento
+#### Null Object
+#### Observer
+#### State
+#### Strategy
+#### Template Method
+#### Visitor
+
 
 ## Structural
     -Concerned with the structure (e.g., class members)
     -Many patterns are wrappers that mimic underlying class' interface
     -Stress the importance of good API design
-#### Adapter  
-#### Bridge  
-#### Composite  
-#### Decorator  
-#### Facade  
-#### Flyweight  
+#### Adapter
+#### Bridge
+#### Composite
+#### Decorator
+#### Facade
+#### Flyweight
 #### Proxy
 
--- src/main/java/patterns/builder/codingexercise/CodeBuilder.java
+++ src/main/java/patterns/builder/codingexercise/CodeBuilder.java
@@ -1,31 +1,57 @@
 package patterns.builder.codingexercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CodeBuilder {
+
+	public static final String PUBLIC = "public ";
+	public static final String CLASS = "class ";
+	public static final String BRACE_OPENING = "{";
+	public static final String BRACE_CLOSING = "}";
+	public static final String SEMICOLON = ";";
+	public static final String SPACE = " ";
+	public static final String NEWLINE = System.lineSeparator();
+
+	List<String> fields;
+	private final String className;
 
 	public CodeBuilder(String className)
 	{
-		// todo
+		this.className = PUBLIC + CLASS + className;
+		this.fields = new ArrayList<>();
 	}
 
 	public CodeBuilder addField(String name, String type)
 	{
-		// todo
+		String field = PUBLIC + type + SPACE + name + SEMICOLON;
+		fields.add(field);
+
+		return this;
 	}
 
 	@Override
 	public String toString()
 	{
-		// todo
+		StringBuilder sb = new StringBuilder();
+		sb.append(className).append(NEWLINE);
+		sb.append(BRACE_OPENING).append(NEWLINE);
+
+		for(String field : fields) {
+			sb.append(String.format("  %s", field)).append(NEWLINE);
+		}
+		sb.append(BRACE_CLOSING).append(NEWLINE);
+
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
 
-		// Sample fo the builder you are asked to create:
-			/*
-				CodeBuilder cb = new CodeBuilder("Person")
-					.addField("name", "String")
-					.addField("age", "int");
-			 */
+		CodeBuilder cb = new CodeBuilder("Person")
+				.addField("name", "String")
+				.addField("age", "int");
+
+		System.out.println(cb);
 
 		// Expected output of above code is:
 			/*
